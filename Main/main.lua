8d0f48dbe5e2441d073e011857e8a48cd76a3b8a
nexus331 <memelordyt53@gmail.com>
2025-12-27 22:30:34 +0800

    networking code update

--- Main/main.lua
+++ Main/main.lua
@@ -1,6 +1,9 @@
 -- START OF PROGRAM -- 
 shell.run("clear")
 print("Program Running...")
+rednet.open("front")
+
+-- SHOW START --
 redstone.setOutput("back", true)
 sleep(3)
 print("Now Playing: Auld Lang Syne")
@@ -8,11 +11,6 @@ redstone.setOutput("back", false)
 print("Firework Launch Sequence Initiated")
 
 -- FIREWORK EXECUTION --
-sleep(67)
-print("Firework1 Launched")
-firework1()
-sleep(13)
-print("Firework3 Launched")
-firework3()
-sleep (4)
-firework2()
+rednet.send(1, A-F1)
+sleep(3)
+rednet.send(2, A-F2)
