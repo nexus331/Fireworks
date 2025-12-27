298a88fd469b29e4abab1e754bf1d24ca239335d
nexus331 <memelordyt53@gmail.com>
2025-12-27 19:20:28 +0800

    main updated

--- Main/main.lua
+++ Main/main.lua
@@ -1,42 +1,11 @@
 -- START OF PROGRAM -- 
 shell.run("clear")
 print("Program Running...")
-redstone.setOutput("left", true)
+redstone.setOutput("back", true)
 sleep(3)
 print("Now Playing: Auld Lang Syne")
-redstone.setOutput("left", false)
+redstone.setOutput("back", false)
 print("Firework Launch Sequence Initiated")
-
--- FIREWORK LAUNCHES --
-local function firework1()
-    redstone.setOutput("right", true)
-    sleep(0.5)
-    redstone.setOutput("right", false)
-end
-
-local function firework2()
-    redstone.setOutput("right", true) -- 1
-    sleep(0.2)
-    redstone.setOutput("right", false)
-    sleep(0.2)
-    redstone.setOutput("right", true)  -- 2
-    sleep(0.2)
-    redstone.setOutput("right", false)
-end
-
-local function firework3()
-    redstone.setOutput("right", true) -- 1
-    sleep(0.2)
-    redstone.setOutput("right", false)
-    sleep(0.2)
-    redstone.setOutput("right", true)  -- 2
-    sleep(0.2)
-    redstone.setOutput("right", false)
-    sleep(0.2)
-    redstone.setOutput("right", true) -- 3
-    sleep(0.2)
-    redstone.setOutput("right", false)
-end
 
 -- FIREWORK EXECUTION --
 sleep(67)
