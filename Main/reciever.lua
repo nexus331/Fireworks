ebf79be453f624d6a9e89e5e4444e4de2e0391af
nexus331 <memelordyt53@gmail.com>
2025-12-29 14:23:21 +0800

    Firework Test #2

--- Main/reciever.lua
+++ Main/reciever.lua
@@ -8,8 +8,14 @@ print("Network Connected.")
 -- FIREWORK Launch Types --
 local function firework1()
     redstone.setOutput("right", true) -- 1
+    redstone.setOutput("left", true)
+    redstone.setOutput("back", true)
+    redstone.setOutput("top", true)
     sleep(0.5)
     redstone.setOutput("right", false)
+    redstone.setOutput("left", false)
+    redstone.setOutput("back", false)
+    redstone.setOutput("top", false)
 end
 
 local function firework2()
@@ -54,6 +60,40 @@ local function firework4()
     redstone.setOutput("top", false)
 end
 
+local function firework5()
+    redstone.setOutput("left", true) -- 1
+    sleep(0.2)
+    redstone.setOutput("left", false)
+    sleep(0.2)
+    redstone.setOutput("right", true)  -- 2
+    sleep(0.2)
+    redstone.setOutput("right", false)
+    sleep(0.2)
+    redstone.setOutput("back", true) -- 3
+    sleep(0.2)
+    redstone.setOutput("back", false)
+    sleep(0.2)
+    redstone.setOutput("top", true) -- 4
+    sleep(0.2)
+    redstone.setOutput("top", false)
+    sleep(0.2) -- REVERSE --
+    redstone.setOutput("top", true) -- 1
+    sleep(0.2)
+    redstone.setOutput("top", false)
+    sleep(0.2)
+    redstone.setOutput("back", true)  -- 2
+    sleep(0.2)
+    redstone.setOutput("back", false)
+    sleep(0.2)
+    redstone.setOutput("right", true) -- 3
+    sleep(0.2)
+    redstone.setOutput("right", false)
+    sleep(0.2)
+    redstone.setOutput("left", true) -- 4
+    sleep(0.2)
+    redstone.setOutput("left", false)
+end
+
 -- Program INIT
 shell.run("clear")
 print("Fireworks Show Software")
@@ -69,10 +109,13 @@ while true do
 
     if fmsg == "f1" then
         print("F1 Activated")
+        firework1()
     elseif fmsg == "f2" then
         print("F2 Activated")
+        firework2()
     elseif fmsg == "f3" then 
         print("F3 Activated")
+        firework3()
     elseif fmsg == "f4" then
         print("F4 Activated")
         firework4()
